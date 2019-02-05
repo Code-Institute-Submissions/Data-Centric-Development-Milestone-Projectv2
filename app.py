9cb50c1cbf9fdad10f3daf8aa2eed7bd0534f06b
Jan Brits <pcerservices@gmail.com>
2019-02-05 13:13:37 +0000

    Struggling with ingredient list, playing around with options

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
 from bson import ObjectId
 
 app = Flask(__name__)
@@ -17,6 +18,24 @@ def index():
 @app.route('/list_recipes')
 def list_recipes():
     return render_template('listrecipes.html', recipes = mongo.db.recipes.find())
+    
+@app.route('/show_recipe/<recipe_id>/')
+def show_recipe(recipe_id):
+    recipe_id = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    entries = recipe_id.items()
+    
+    return render_template('showrecipe.html', entries = entries)
+    
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('addrecipe.html', cuisine=mongo.db.cuisine.find(), user=mongo.db.user.find())
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    print(request.form)
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('list_recipes'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
